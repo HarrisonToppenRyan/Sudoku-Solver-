3eabc035d4085b2b0adc4677a03bb646b76edcc8
Harrison Toppen-Ryan <harrison619@live.com>
2020-08-04 19:05:53 -0700

    Add files via upload

---
+++ Sudoku.py
@@ -0,0 +1,22 @@
+import pprint
+
+def solve(bo):
+    find = find_empty(bo)
+    if find:
+        row, col = find 
+    else:
+        return True
+
+    for i in range(1,10):
+        if valid(bo, (row, col), i):
+            bo[row][col] = i 
+
+            if solve(bo):
+                return True
+            
+            bo[row][col] = 0
+    return False
+
+
+
+     
